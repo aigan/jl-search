853e26ad77063352e3ca13ec7e1ed95f27a3da3e
Jonas Liljegren <jonas@liljegren.org>
2024-03-20 14:55:48 +0100

    Semantic html and animations

--- jl-search.mjs
+++ jl-search.mjs
@@ -9,6 +9,14 @@ let element_id = 0;
 const collator = new Intl.Collator("sv");
 const numformat = new Intl.NumberFormat(); // Use locale
 
+// Since event targes are disabled during view transitions, we are
+// catching clicks here and replaying them after transition finished.
+const global_events = [];
+function global_onclick(ev) {
+  // log("catched click", ev, ev.target);
+  global_events.push(ev);
+}
+
 class El extends HTMLElement {
   static is = "jl-search"
   static states = {
@@ -38,19 +46,21 @@ class El extends HTMLElement {
     $el._id = ++element_id;
     $el._li_id = 0;
     $el._ev = {};
-    $el._selected_index = null;
-    $el._accepted = false;
     $el._error = null;
     $el._req_id = 0;
     $el._memory = new Map();
     $el._caps = caps;
+    $el._transitions = new Set();
+    $el._transitions_resolve = null;
+    $el._page_size = 8;
 
-    $el._searching = false;
-    $el._mouse_inside = false;
+    $el._selected_index = null;
+    $el._accepted = false;
     $el._retain_opened = false;
 
-    $el._page_size = 8;
-    // $el._page_size = 4;
+    // Keeping async actions straight
+    $el._searching = false;
+    $el._mouse_inside = false;
 
     const data_tmpl = {
       options: [],
@@ -66,9 +76,9 @@ class El extends HTMLElement {
     }
 
     $el._$inp = $el.querySelector("input");
-    $el._$opts = $el.querySelector(".options");
-    $el._$label = $el.querySelector(".input-wrapper");
-    $el._$feedback = $el.querySelector(".feedback");
+    $el._$opts = $el.querySelector("nav");
+    $el._$label = $el.querySelector("fieldset");
+    $el._$feedback = $el.querySelector("nav footer");
     $el._$state = $el.querySelector(".state");
 
 
@@ -92,11 +102,93 @@ class El extends HTMLElement {
   setup_$opts() {
     const $el = this;
     const $opts = $el._$opts;
+
     $opts.setAttribute("popover", "manual");
     $opts.setAttribute("anchor", $el._$label.id);
-    caps.popover.then(() =>
-      $opts.classList.add('loaded-popover')
-    );
+    $opts.classList.add("empty");
+
+    caps.popover.then(() => {
+      $opts.classList.add('loaded-popover');
+      // $opts.style.animationIterationCount = "0";
+    });
+
+    let open_promise = null;
+    let open_resolve;
+    $opts.get_open_promise = () => {
+      // Can't use only :popover-open since it might not have finished open?
+      // Or did I miss something
+      // if ($opts.matches(":popover-open") && $opts.offsetParent) {
+      if ($opts.matches(":popover-open")) {
+        // log("get_open_promise");
+        // log(":popover-open", $el.querySelector(":popover-open"));
+        // log("offsetParent", $opts.offsetParent);
+        return Promise.resolve();
+      }
+
+      if (open_promise) return open_promise;
+      return open_promise = new Promise(resolve => {
+        // log("Creating new open_promise");
+        open_resolve = resolve;
+      });
+    }
+
+    let close_promise = null;
+    let close_resolve;
+    $opts.get_close_promise = () => {
+      if (!$opts.matches(":popover-open")) {
+        // log("get_close_promise");
+        return Promise.resolve();
+      }
+
+      if (close_promise) return close_promise;
+      return close_promise = new Promise(resolve => {
+        // log("Creating new close_promise");
+        close_resolve = resolve;
+      });
+    }
+
+    // $opts.addEventListener("transitionstart", ev => log("start"));
+    // $opts.addEventListener("transitionend", ev => log("end"));
+    // $opts.addEventListener("transitioncancel", ev => log("cancel"));
+    // $opts.addEventListener("transitionrun", ev => log("run"));
+
+    $opts.addEventListener("transitionend", ev => {
+      if ($el.dataset.anim === "opening") {
+        log("transitioned to opened");
+        if (!open_promise) return;
+        open_promise = null;
+        open_resolve();
+        return;
+      }
+
+      if ($el.dataset.anim === "closing") {
+        log("transitioned to closed");
+        if (!close_promise) return;
+        close_promise = null;
+        close_resolve();
+        return;
+      }
+
+      log("unexpected transition");
+    });
+
+
+    $opts.addEventListener("toggle", ev => {
+      return;
+      if (ev.newState === "open") {
+        log("$opts toggle to opened");
+        if (!open_promise) return;
+        open_promise = null;
+        open_resolve();
+      } else {
+        log("$opts toggle to closed");
+        if (!close_promise) return;
+        close_promise = null;
+        close_resolve();
+      }
+    });
+
+
   }
 
   static input_defaults = {
@@ -142,21 +234,25 @@ class El extends HTMLElement {
     }
   }
 
-  on_state_click(ev) {
+  async on_state_click(ev) {
     const $el = this;
+    ev.stopPropagation(); // do $el.onclick after
+
     const state = $el.dataset.state;
-    // log("Click on state", state, "toggle");
+    // log("Click on state", state, "toggle", ev);
     const opened = $el.hasAttribute("opened");
 
     if (opened) {
       $el._retain_opened = false;
-      return $el.hide_options();
+      await $el.hide_options();
     } else {
       $el._retain_opened = true;
       $el.render_feedback($el._data.rendered);
-      $el.show_options();
-      return;
+      await $el.show_options();
     }
+
+    // Manually propagate click after all else is done
+    $el.on_click(ev);
   }
 
   on_click(ev) {
@@ -184,6 +280,7 @@ class El extends HTMLElement {
 
   on_mousedown(ev) {
     this._mouse_inside = true;
+    // log("mousedown");
     setTimeout(() => { this._mouse_inside = false });
   }
 
@@ -211,7 +308,7 @@ class El extends HTMLElement {
   on_focus(ev) {
     const $el = this;
     const $inp = $el._$inp;
-    // log("input focused", $el._retain_opened);
+    // log("input focused", $el._retain_opened, $el._mouse_inside);
 
     if (!navigator.userActivation.hasBeenActive) return;
 
@@ -321,8 +418,8 @@ class El extends HTMLElement {
     // during the preparations.
 
     try {
-      // $el.startViewTransition(() =>
-        $el.render_options({
+      // $el.startViewTransition("render options",() =>
+      $el.render_options({
         ...res,
 
         current_req: $el._req_id, // Searching
@@ -330,7 +427,7 @@ class El extends HTMLElement {
         prepared_req: res.req, // Rendering
         previous_req: $el._rendered_req,
         has_focus: $el.has_focus(),
-        })
+      })
       // );
       $el._data.rendered = res;
     } catch (error) {
@@ -371,6 +468,21 @@ class El extends HTMLElement {
 
   // override for custom layout
   render_options({ found = [], error, count = 0, speed, query }) {
+    /*
+    Callbacks for handling animations
+
+    enter_before
+    enter_anim_before returns Promise
+    enter_anim_after
+    (enter_after)
+
+    (exit_before)
+    exit_anim_before returns Promise
+    exit_anim_after
+    exit_after
+    
+    */
+
     const $el = this;
     const max = Math.min(found.length, $el._page_size);
     // log("render options from", found);
@@ -381,22 +493,25 @@ class El extends HTMLElement {
     const removed = [];
 
     const $ul = $el.querySelector("ul");
-    const children = $ul.children;
+    const children = [...$ul.children]
+      .filter($li => (!$li.classList.contains('exit')));
     let i = 0;
-    for (const $child of [...children]) {
+    for (const $child of children) {
       const val = $child.dataset.text;
+      // log("consider", $child);
+      if ($child.classList.contains("exit")) console.warn("DANGER");
       while (i < max) {
         const order = collator.compare(val, found[i]);
         // log("compare", val, "with", i, found[i], order);
 
         if (order === -1) {
           // log("  remove");
-          $child.remove();
           removed.push($child);
           break;
         } else if (order === +1) {
           // log("  insert", found[i], "before", val);
           const $new = $el.render_item(found[i]);
+          // $el.enter_before($new);
           $ul.insertBefore($new, $child);
           added.push($new);
         } else {
@@ -408,23 +523,88 @@ class El extends HTMLElement {
       }
     }
 
+    // Append the rest of the items found
     for (let j = i; j < max; j++) {
       // log("append", found[j]);
       const $new = $el.render_item(found[j]);
+      // $el.enter_before($new);
       $ul.append($new);
       added.push($new);
     }
 
-    while ($ul.children.length > max) {
-      removed.push($ul.children[max]);
-      $ul.children[max].remove();
+    // Remove previous items pushed beyond page limit
+
+    // log("existing children", children.length);
+    // log("added", added.length);
+    // log("removed", removed.length);
+    // log("pagesize", $el._page_size);
+
+    const pos = - added.length + removed.length + max;
+
+    // log("remove from", pos, "to", children.length);
+    for (let i = pos; i < children.length; i++) {
+      // log("should remove overflow child", i);
+      const $child = children[i];
+      removed.push($child);
     }
 
-    // for (const $child of removed) { 
-    //   $child.remove();
-    // }
+    $el.enter_anim_before(added);
+    $el.exit_anim_before(removed).then(() => {
+      // $el.exit_anim_after(removed);
+      for (const $child of removed) $child.remove();
+      // $el.exit_after();
+    });
 
     $el.render_feedback({ error, count, speed, query });
+  }
+
+  async enter_anim_before(elements) {
+    const $el = this;
+    if (!$el.hasAttribute("more-anim")) return Promise.resolve();
+    await $el._$opts.get_open_promise();
+    // log("$opts opened", $el._$opts.offsetParent);
+
+    for (const $li of elements) {
+      const rect = $li.getBoundingClientRect();
+      const height_goal = rect.height;
+      if (!height_goal) throw Error("Could not get item height");
+      // log("item", $li, $li.parentElement, "start", rect.height);
+      const style = $li.style;
+      style.height = "0px";
+      style.overflow = "hidden";
+      requestAnimationFrame(() => {
+        style.transition = "height .3s";
+        style.height = height_goal + "px";
+        $li.addEventListener('transitionend', () => {
+          delete style.height;
+          delete style.overflow;
+        }, { once: true });
+      });
+    }
+  }
+
+  exit_anim_before(elements) {
+    const $el = this;
+    if (!$el.hasAttribute("more-anim")) return Promise.resolve();
+    return new Promise((resolve, reject) => {
+      for (const $li of elements) {
+        const height_start = $li.scrollHeight;
+        if (!height_start) throw Error("Could not get item height");
+        const style = $li.style;
+        $li.classList.add("exit");
+        requestAnimationFrame(() => {
+          style.height = height_start + "px";
+          requestAnimationFrame(() => {
+            style.transition = "height .3s";
+            style.height = "0px";
+          });
+        });
+
+        $li.addEventListener('transitionend', () => {
+          resolve();
+        }, { once: true });
+      }
+    });
   }
 
   render_item(text) {
@@ -482,7 +662,7 @@ class El extends HTMLElement {
     const $el = this;
     if ($el._searching) return "Fetching search result . . .";
     if (!query.length) return "Try typing some letters";
-    if (count === 1) return `Found one measly results after ${speed / 1000} seconds`;
+    if (count === 1) return `Found one measly result after ${speed / 1000} seconds`;
     if (count > 0) return `Found ${numformat.format(count)} results in ${speed / 1000} seconds`;
     return `Found absolutely nothing after searching for ${speed / 1000} seconds`;
   }
@@ -490,35 +670,117 @@ class El extends HTMLElement {
   async show_options() {
     const $el = this;
 
+    if ($el.hasAttribute("opened") && !$el.dataset.anim) return;
+
+    // The process can change direction at any time
+    $el.dataset.anim = "opening";
+
     // console.warn("OPEN options", $el._data.found.found.length);
     await caps.popover;
     await caps.anchor_positioning; // wait on slow polyfill loads
-    if ($el.hasAttribute("opened")) return;
+    if ($el.dataset.anim !== "opening") return;
 
-    $el.startViewTransition(() => {
+    // Firefox popover animation workaround
+    // $el._$opts.style.animationIterationCount = "1";
 
-      $el.setAttribute("opened", "");
+    log("show_options set opened");
+    await $el.toggle_options(true);
+    if ($el.dataset.anim !== "opening") return;
+
+    // May have changed async
+    log("show_options popover");
+    $el._$opts.togglePopover(true);
+
+    delete $el.dataset.anim;
+  }
+
+  async toggle_options(force) {
+    const $el = this;
+
+    // Skip animations on larger screens. Using the same breakpoint as the
+    // css. (Since small screen triggers movement of the field to the top of
+    // screen.)
+
+    const should_animate = window.matchMedia('(max-width: 30rem)').matches;
+    if (should_animate) {
+      await $el.startViewTransition("toggle_options", () => {
+        $el.toggleAttribute("opened", force);
+        $el.update_state();
+      });
+    } else {
+      $el.toggleAttribute("opened", force);
       $el.update_state();
-      $el._$opts.showPopover();
+    }
+  }
 
+  startViewTransition(label, fn) {
+    if (!document.startViewTransition) return fn();
+    const $el = this;
+
+    const transition = document.startViewTransition(fn);
+    log("view transition", label);
+
+    if (!$el._transitions.size) {
+      log("adding global_onclick listener");
+      document.addEventListener("click", global_onclick);
+      const done = new Promise(resolve => {
+        $el._transitions_resolve = resolve;
+      });
+
+      done.then(() => {
+        log("all view transition finished");
+        if ($el._transitions.size) throw Error("nor really done");
+
+        document.removeEventListener("click", global_onclick);
+
+        // Just in case something adds stuff during loop
+        const events = [...global_events];
+        global_events.length = 0;
+
+        for (const ev of events) {
+          if (ev.target !== document.documentElement) continue;
+          const $target = document.elementFromPoint(
+            ev.clientX, ev.clientY);
+
+          if ($el.contains($target)) continue;
+
+          log("replay event to", $target.tagName);
+          $target.dispatchEvent(ev);
+        }
+      });
+    }
+
+    $el._transitions.add(transition);
+
+    return transition.finished.then(() => {
+      $el._transitions.delete(transition);
+      log("view transition", label, "finished");
+      if ($el._transitions.size) return;
+      $el._transitions_resolve();
     });
   }
 
-  startViewTransition(fn) {
-    log("view transition");
-    if (!document.startViewTransition) return fn();
-    document.startViewTransition(fn);
-  }
-
-  hide_options() {
+  async hide_options() {
     const $el = this;
     // log("hide");
-  
-    $el.startViewTransition(() => {
-      $el.removeAttribute("opened");
-      $el.update_state();
-      $el._$opts.hidePopover();
-    });
+
+    if (!$el.hasAttribute("opened") && !$el.dataset.anim) return;
+
+    // The process can change direction at any time
+    $el.dataset.anim = "closing";
+
+    log("hide_options popver");
+    const closed = $el._$opts.get_close_promise();
+    await closed;
+
+    if ($el.dataset.anim !== "closing") return;
+    $el._$opts.hidePopover();
+
+    log("hide_options set closed");
+    await $el.toggle_options(false);
+
+    log("hide_options done");
+    delete $el.dataset.anim;
   }
 
   show_error() {
@@ -631,7 +893,7 @@ function load_caps(caps) {
   caps.polyfilled = {};
   caps.popover = load_polyfill_popover();
   caps.anchor_positioning = load_polyfill_anchor_positioning();
-  // caps.scroll_into_view = load_polyfill_scroll_into_view();
+  caps.scroll_into_view = load_polyfill_scroll_into_view();
   caps.user_activation = load_polyfill_user_activation();
 
   // Also using startViewTransition from View Transitions API
@@ -650,10 +912,26 @@ function load_caps(caps) {
 
 }
 
-function load_polyfill_popover() {
-  if (HTMLElement.prototype.togglePopover) return Promise.resolve();
+async function load_polyfill_popover() {
+  if (HTMLElement.prototype.togglePopover) return;
   caps.polyfilled.popover = true;
-  return import("https://cdn.jsdelivr.net/npm/@oddbird/popover-polyfill@latest");
+  await import("https://cdn.jsdelivr.net/npm/@oddbird/popover-polyfill@latest");
+  return;
+
+  // Hack for handling the polyfill style specificity
+  const popover_css = new CSSStyleSheet();
+  await popover_css.replace(`
+  [popover]:not(.\\:popover-open) { 
+    height: 0;
+   }
+  `);
+
+  // transform: scaleY(0);
+  // display: none;
+  // display: flex;
+
+  document.adoptedStyleSheets.push(popover_css);
+  return;
 }
 
 async function load_polyfill_anchor_positioning() {
